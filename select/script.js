2d3ef6d9150d078569b4c40285e13688a8f8c0bd
Thays Martines <thaysmartines18@gmail.com>
2024-01-13 22:59:39 -0400

    select customizado

---
+++ select/script.js
@@ -0,0 +1,20 @@
+lucide.createIcons()
+
+let select = document.querySelector('.select'),
+selectedValue = document.getElementById('selected-value'),
+optionsViewButton = document.getElementById('options-view-button'),
+inputsOptions = document.querySelectorAll('.option input')
+
+inputsOptions.forEach(input => {
+input.addEventListener('click', event => {
+    selectedValue.textContent = input.dataset.label
+
+    const isMouseOrTouch = 
+    event.pointerType = "mouse"  
+    event.pointerType = "touch"
+
+    isMouseOrTouch && optionsViewButton.click()
+
+    })
+})
+
